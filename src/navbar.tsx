9d87b38e537e100f32db75bbc4bf764729e4f522
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-11-14 12:27:01 -1000

    where did that file name come from? I have no idea

--- src/navbar.tsx
+++ src/navbar.tsx
@@ -8,7 +8,7 @@ function Navbar() {
   return (
     <>
       <div>
-        <img src="./src/assets/images/logo.png" className="AppLogo"></img>
+        <img src="./assets/logo-19f49d27.png" className="AppLogo"></img>
        <nav>
         <ul>
           <li className="navbar">Home</li>
