01f0ccb36cec237845bab7d291f13a7ca74b1287
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-11-14 09:32:12 -1000

    bootstrap Card import

--- src/specials.tsx
+++ src/specials.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Card from 'react-bootstrap/Card'
 
 
 function Specials() {
@@ -10,24 +11,24 @@ function Specials() {
         <header>
             <h1>This week's specials!</h1>
             <button>Online Menu</button>
-            <card>
+            <Card>
                 <img></img>
                 <h2>Greek Salad</h2>
                 <h2>$12.95</h2>
                 <p></p>
-                </card>
-            <card>
+                </Card>
+            <Card>
                 <img></img>
                 <h2>Bruschetta</h2>
                 <h2>$5.99</h2>
                 <p></p>
-                </card>
-            <card>
+                </Card>
+            <Card>
                 <img></img>
                 <h2>Lemon Dessert</h2>
                 <h2>$5.00</h2>
                 <p></p>
-                </card>
+                </Card>
         </header>
         </div>
     </>
