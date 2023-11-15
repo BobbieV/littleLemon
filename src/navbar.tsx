20b3dd3ce6e66653d36c8fbf338f15dfb887c486
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-11-14 14:19:33 -1000

    put classname back on the navbar list

--- src/navbar.tsx
+++ src/navbar.tsx
@@ -11,12 +11,12 @@ function Navbar() {
         <img style={{width: '8rem', height: '3rem', position: 'relative'}} src="./assets/logo-19f49d27.png" className="AppLogo"></img>
        <nav>
         <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Menu</li>
-          <li>Reservations</li>
-          <li>Order Online</li>
-          <li>Login</li>
+          <li className="navbar">Home</li>
+          <li className="navbar">About</li>
+          <li className="navbar">Menu</li>
+          <li className="navbar">Reservations</li>
+          <li className="navbar">Order Online</li>
+          <li className="navbar">Login</li>
         </ul>
        </nav>
         </div>
