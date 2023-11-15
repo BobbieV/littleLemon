3a1c79670056776825508394cd4221bae6acb88d
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-11-14 14:48:31 -1000

    still tryna get this dang ol image to work

--- src/Header.tsx
+++ src/Header.tsx
@@ -12,7 +12,7 @@ function Header() {
             <h1 className="title">Little Lemon</h1>
             <h2 className="text">Chicago</h2>
             <p className="text">We are a family-owned Mediterranean restaurant, focused on traditional recipes, served with a modern twist.</p>
-            <img src="src/assets/images/Hero Image.png" className="heroImage"></img>
+            <img src="https://github.com/BobbieV/littleLemon/blob/main/src/assets/images/Hero%20Image.png?raw=true" className="heroImage"></img>
             <button className="reserveATable">Reserve a Table</button>
         </header>
         </div>
