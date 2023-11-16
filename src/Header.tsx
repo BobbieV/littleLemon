fd4cf657d03f66c8c4f45c0412e15a8f7068811c
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-11-16 08:31:32 -1000

    switch .reserveATable and .heroImage

--- src/Header.tsx
+++ src/Header.tsx
@@ -12,8 +12,8 @@ function Header() {
             <h1 className="title">Little Lemon</h1>
             <h2 className="text">Chicago</h2>
             <p className="text">We are a family-owned Mediterranean restaurant, focused on traditional recipes, served with a modern twist.</p>
-            <img src="https://github.com/BobbieV/littleLemon/blob/main/src/assets/images/Hero%20Image.png?raw=true" className="heroImage"></img>
             <button className="reserveATable">Reserve a Table</button>
+            <img src="https://github.com/BobbieV/littleLemon/blob/main/src/assets/images/Hero%20Image.png?raw=true" className="heroImage"></img>
         </header>
         </section>
     </>
