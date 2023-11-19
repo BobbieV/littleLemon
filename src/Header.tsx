02ff7a1d1ad1a353bd14976aab6452783c77d751
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-11-19 13:16:56 -1000

    distinguish h2text ptext and text in .header

--- src/Header.tsx
+++ src/Header.tsx
@@ -9,8 +9,8 @@ function Header() {
       <section className="header">
         <header>
             <h1 className="title">Little Lemon</h1>
-            <h2 className="text">Chicago</h2>
-            <p className="text">We are a family-owned Mediterranean restaurant, focused on traditional recipes, served with a modern twist.</p>
+            <h2 className="h2text text">Chicago</h2>
+            <p className="text ptext">We are a family-owned Mediterranean restaurant, focused on traditional recipes, served with a modern twist.</p>
             <button className="reserveATable">Reserve a Table</button>
             <img src="https://github.com/BobbieV/littleLemon/blob/main/src/assets/images/Hero%20Image.png?raw=true" className="heroImage"></img>
         </header>
