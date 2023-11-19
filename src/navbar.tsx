ce0ebfdf8d2ffa290e2953ecc429edeba74ab7f2
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-11-19 12:28:57 -1000

    create .navtext class for <ul>

--- src/navbar.tsx
+++ src/navbar.tsx
@@ -10,13 +10,13 @@ function Navbar() {
       <section className="nav">
         <nav>
           <img style={{width: '8rem', height: '3rem'}} src="./assets/logo-19f49d27.png" className="AppLogo navbar"></img>
-        <ul>
-          <li className="navbar navtext">Home</li>
-          <li className="navbar navtext">About</li>
-          <li className="navbar navtext">Menu</li>
-          <li className="navbar navtext">Reservations</li>
-          <li className="navbar navtext">Order Online</li>
-          <li className="navbar navtext">Login</li>
+        <ul classname="navtext">
+          <li className="navbar navtext1">Home</li>
+          <li className="navbar navtext2">About</li>
+          <li className="navbar navtext3">Menu</li>
+          <li className="navbar navtext4">Reservations</li>
+          <li className="navbar navtext5">Order Online</li>
+          <li className="navbar navtext6">Login</li>
         </ul>
        </nav>
         </section>
