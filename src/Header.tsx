0b5632d91dfc92bf8e5b21cb46935070cc677979
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-11-21 09:17:06 -1000

    fix wrappers

--- src/Header.tsx
+++ src/Header.tsx
@@ -6,15 +6,16 @@ function Header() {
 
   return (
     <>
-      <section className="headerWrapper">
         <header id="header">
-            <h1 className="title">Little Lemon</h1>
+            <section className="textWrapper"><h1 className="title">Little Lemon</h1>
             <h2 className="h2text text">Chicago</h2>
             <p className="text ptext">We are a family-owned Mediterranean restaurant, focused on traditional recipes, served with a modern twist.</p>
             <button className="reserveATable">Reserve a Table</button>
+            </section>
+            <section className="heroImageWrapper">
             <img src="https://github.com/BobbieV/littleLemon/blob/main/src/assets/images/Hero%20Image.png?raw=true" className="heroImage"></img>
-        </header>
         </section>
+        </header>
     </>
   )
 }
