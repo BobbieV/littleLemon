8ef36a0faf7c8d2c4709ea73d108058de6e9aa2a
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-11-21 07:58:24 -1000

    edit navbar.tsx html structure. remove most of .navbar css

--- src/navbar.tsx
+++ src/navbar.tsx
@@ -7,21 +7,21 @@ function Navbar() {
 
   return (
     <>
-      <section className="navWrapper">
         <nav className="nav">
-          <div className="AppLogoWrapper">
+          <section className="AppLogoWrapper">
           <img src="./assets/logo-19f49d27.png" className="AppLogo navbar"></img>
-          </div>
+          </section>
+          <section className="navtextWrapper">
         <ul className="navtext">
-          <li className="navbar navtext1">Home</li>
-          <li className="navbar navtext2">About</li>
-          <li className="navbar navtext3">Menu</li>
-          <li className="navbar navtext4">Reservations</li>
-          <li className="navbar navtext5">Order Online</li>
-          <li className="navbar navtext6">Login</li>
+          <li className="navbar navtextHome">Home</li>
+          <li className="navbar navtextAbout">About</li>
+          <li className="navbar navtextMenu">Menu</li>
+          <li className="navbar navtextReservations">Reservations</li>
+          <li className="navbar navtextOrderonline">Order Online</li>
+          <li className="navbar navtextLogin">Login</li>
         </ul>
-       </nav>
         </section>
+       </nav>
     </>
   )
 }
