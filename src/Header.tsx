d43a35a90b5e2c995a6df154228c1af58f532b07
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-11-22 07:40:56 -1000

    .reserveATableWrapper margin auto auto and .ptext width up to 60%

--- src/Header.tsx
+++ src/Header.tsx
@@ -10,7 +10,9 @@ function Header() {
             <section className="textWrapper"><h1 className="title">Little Lemon</h1>
             <h2 className="h2text text">Chicago</h2>
             <p className="text ptext">We are a family-owned Mediterranean restaurant, focused on traditional recipes, served with a modern twist.</p>
+            <div className="reserveATableWrapper">
             <button className="reserveATable">Reserve a Table</button>
+            </div>
             </section>
             <section className="heroImageWrapper">
             <img src="https://github.com/BobbieV/littleLemon/blob/main/src/assets/images/Hero%20Image.png?raw=true" className="heroImage"></img>
