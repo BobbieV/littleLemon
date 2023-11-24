4e0686c3388ed651cadf41c7d91f159de4f91510
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-11-24 10:38:11 -1000

    improve semantic html structure of specials.tsx

--- src/specials.tsx
+++ src/specials.tsx
@@ -6,30 +6,32 @@ function Specials() {
 
   return (
     <>
-      <div className="specialsWrapper">
-        <header>
-            <h1>This week's specials!</h1>
-            <button>Online Menu</button>
-            <section>
-                <img></img>
-                <h2>Greek Salad</h2>
-                <h2>$12.95</h2>
-                <p></p>
-                </section>
-            <section>
-                <img></img>
-                <h2>Bruschetta</h2>
-                <h2>$5.99</h2>
-                <p></p>
-                </section>
-            <section>
-                <img></img>
-                <h2>Lemon Dessert</h2>
-                <h2>$5.00</h2>
-                <p></p>
-                </section>
+        <header id="#specialsWrapper">
+          <section className="topWrapper">
+            <h1 className="h1Specials">This week's specials!</h1>
+            <button className="buttonOM">Online Menu</button>
+            </section>
+            <section className="bottomWrapper">
+              <article className="menuItem greekSalad">
+                <img className="menuItemImage imgGreekSalad"></img>
+                <h2 className="label labelGreekSalad">Greek Salad</h2>
+                <h2 className="price priceGreekSalad">$12.95</h2>
+                <p className="description descriptionGreekSalad"></p>
+                </article>
+            <article className="menuItem bruschetta">
+                <img className="menuItemImage imgBruschetta"></img>
+                <h2 className="label labelBruschetta">Bruschetta</h2>
+                <h2 className="price priceBruschetta">$5.99</h2>
+                <p className="description descriptionBruschetta"></p>
+                </article>
+            <article className="menuItem lemonDessert ">
+                <img className="menuItemImage imageLemonDessert"></img>
+                <h2 className="label labelLemonDessert">Lemon Dessert</h2>
+                <h2 className="price priceLemonDessert">$5.00</h2>
+                <p className="description descriptionLemonDessert"></p>
+                </article>
+            </section>
         </header>
-        </div>
     </>
   )
 }
