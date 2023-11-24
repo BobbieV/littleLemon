6dd802e37822786fdccb7292903ac8400bf1dd27
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-11-24 11:23:29 -1000

    add .label to .price

--- src/specials.tsx
+++ src/specials.tsx
@@ -15,19 +15,19 @@ function Specials() {
               <article className="menuItem greekSalad">
                 <img className="menuItemImage imgGreekSalad" src="https://github.com/BobbieV/littleLemon/blob/main/src/assets/images/greek salad.jpg?raw=true"></img>
                 <h2 className="label labelGreekSalad">Greek Salad</h2>
-                <h2 className="price priceGreekSalad">$12.95</h2>
+                <h2 className="label price priceGreekSalad">$12.95</h2>
                 <p className="description descriptionGreekSalad"></p>
                 </article>
             <article className="menuItem bruschetta">
                 <img className="menuItemImage imgBruschetta" src="https://github.com/BobbieV/littleLemon/blob/90e6980f8559c03500120c4b290dd6482b8a16c9/src/assets/images/bruschetta.png?raw=true"></img>
                 <h2 className="label labelBruschetta">Bruschetta</h2>
-                <h2 className="price priceBruschetta">$5.99</h2>
+                <h2 className="label price priceBruschetta">$5.99</h2>
                 <p className="description descriptionBruschetta"></p>
                 </article>
             <article className="menuItem lemonDessert ">
                 <img className="menuItemImage imageLemonDessert" src="https://github.com/BobbieV/littleLemon/blob/main/src/assets/images/lemon dessert.jpg?raw=true"></img>
                 <h2 className="label labelLemonDessert">Lemon Dessert</h2>
-                <h2 className="price priceLemonDessert">$5.00</h2>
+                <h2 className="label price priceLemonDessert">$5.00</h2>
                 <p className="description descriptionLemonDessert"></p>
                 </article>
             </section>
