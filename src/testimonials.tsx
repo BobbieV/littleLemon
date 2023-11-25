2c8d4b6e008b2de979cfe932db3b319718e35bb7
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-11-24 17:05:29 -1000

    add links to user profile pic images

--- src/testimonials.tsx
+++ src/testimonials.tsx
@@ -10,7 +10,7 @@ function Testimonials() {
         <header id="testimonialsWrapper">
             <h1 className="h1Testimonials">Testimonials</h1>
             <section className="testimonialContainer testimonial1">
-              <img className="testimonialUserImage testimonialUserImage1 testimonialUserImageJoan"></img>
+              <img className="testimonialUserImage testimonialUserImage1 testimonialUserImageJoan" src="https://github.com/BobbieV/littleLemon/blob/88e414dfd6b3636026a90dc465f15aaafc032289/src/assets/images/'Joan'%20Female%20Profile%20Pic%2075%20x%2075px.png?raw=true"></img>
               <hgroup className="h2TestimonialText">
                 <h2 className="name name1">Joan</h2>
                 <h2 className="rating rating1">9/10</h2>
@@ -18,7 +18,7 @@ function Testimonials() {
               <p className="userFeedback userFeedback1"></p>
              </section>
             <section className="testimonialContainer testimonial2">
-            <img className="testimonialUserImage testimonialUserImage2 testimonialUserImageDiane"></img>
+            <img className="testimonialUserImage testimonialUserImage2 testimonialUserImageDiane" src="https://github.com/BobbieV/littleLemon/blob/88e414dfd6b3636026a90dc465f15aaafc032289/src/assets/images/'Diane'%20Female%20Profile%20Pic%2075%20x%2075px.png?raw=true"></img>
               <hgroup className="h2TestimonialText">
                 <h2 className="rating rating2">10/10</h2>
                 <h2 className="name name1">Diane</h2>
@@ -26,15 +26,15 @@ function Testimonials() {
               <p className="userFeedback userFeedback2"></p>
             </section>
             <section className="testimonialContainer testimonial3">
-            <img className="testimonialUserImage testimonialUserImage3 testimonialUserImageDavid"></img>
+            <img className="testimonialUserImage testimonialUserImage3 testimonialUserImageDavid" src="https://github.com/BobbieV/littleLemon/blob/88e414dfd6b3636026a90dc465f15aaafc032289/src/assets/images/'David'%20Male%20Profile%20Pic%2075%20x%2075px.png?raw=true"></img>
               <hgroup className="h2TestimonialText">
                 <h2 className="rating rating3">9/10</h2>
-                <h2 className="name name1">Diane</h2>
+                <h2 className="name name1">David</h2>
               </hgroup>
               <p className="userFeedback userFeedback3"></p>
             </section>
             <section className="testimonialContainer testimonial4">
-            <img className="testimonialUserImage testimonialUserImage4 testimonialUserImageJenna"></img>
+            <img className="testimonialUserImage testimonialUserImage4 testimonialUserImageJenna" src="https://github.com/BobbieV/littleLemon/blob/88e414dfd6b3636026a90dc465f15aaafc032289/src/assets/images/'Jenna'%20Female%20Profile%20Pic%2075%20x%2075px.png?raw=true"></img>
               <hgroup className="h2TestimonialText">
                 <h2 className="rating rating4">8/10</h2>
                 <h2 className="name name1">Jenna</h2>
