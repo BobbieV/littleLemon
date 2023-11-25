975f4d6c577af6d19ce2b60a7fb5989780a1a4db
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-11-24 20:08:27 -1000

    fix upside down joan ratings

--- src/testimonials.tsx
+++ src/testimonials.tsx
@@ -13,8 +13,8 @@ function Testimonials() {
             <section className="testimonialContainer testimonial1">
               <img className="testimonialUserImage testimonialUserImage1 testimonialUserImageJoan" src="https://github.com/BobbieV/littleLemon/blob/88e414dfd6b3636026a90dc465f15aaafc032289/src/assets/images/'Joan'%20Female%20Profile%20Pic%2075%20x%2075px.png?raw=true"></img>
               <hgroup className="h2TestimonialText">
+              <h2 className="rating rating1">9/10</h2>
                 <h2 className="name name1">Joan</h2>
-                <h2 className="rating rating1">9/10</h2>
               </hgroup>
               <p className="userFeedback userFeedback1">The food was outstanding and the atmosphere was lovely. The service was great too, but it was a little pricey. Overall, a fantastic experience.</p>
              </section>
