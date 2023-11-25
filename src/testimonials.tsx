08571ab916d52a2250657b16e429f627969191e9
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-11-24 16:59:25 -1000

    create name element and associated classes

--- src/testimonials.tsx
+++ src/testimonials.tsx
@@ -10,30 +10,34 @@ function Testimonials() {
         <header id="testimonialsWrapper">
             <h1 className="h1Testimonials">Testimonials</h1>
             <section className="testimonialContainer testimonial1">
-              <img className="testimonialUserImage testimonialUserImage1"></img>
+              <img className="testimonialUserImage testimonialUserImage1 testimonialUserImageJoan"></img>
               <hgroup className="h2TestimonialText">
-                  <h2 className="rating rating1">9/10</h2>
+                <h2 className="name name1">Joan</h2>
+                <h2 className="rating rating1">9/10</h2>
               </hgroup>
               <p className="userFeedback userFeedback1"></p>
              </section>
             <section className="testimonialContainer testimonial2">
-            <img className="testimonialUserImage testimonialUserImage2"></img>
+            <img className="testimonialUserImage testimonialUserImage2 testimonialUserImageDiane"></img>
               <hgroup className="h2TestimonialText">
                 <h2 className="rating rating2">10/10</h2>
+                <h2 className="name name1">Diane</h2>
               </hgroup>
               <p className="userFeedback userFeedback2"></p>
             </section>
             <section className="testimonialContainer testimonial3">
-            <img className="testimonialUserImage testimonialUserImage3"></img>
+            <img className="testimonialUserImage testimonialUserImage3 testimonialUserImageDavid"></img>
               <hgroup className="h2TestimonialText">
                 <h2 className="rating rating3">9/10</h2>
+                <h2 className="name name1">Diane</h2>
               </hgroup>
               <p className="userFeedback userFeedback3"></p>
             </section>
             <section className="testimonialContainer testimonial4">
-            <img className="testimonialUserImage testimonialUserImage4"></img>
+            <img className="testimonialUserImage testimonialUserImage4 testimonialUserImageJenna"></img>
               <hgroup className="h2TestimonialText">
                 <h2 className="rating rating4">8/10</h2>
+                <h2 className="name name1">Jenna</h2>
               </hgroup>
               <p className="userFeedback userFeedback4"></p>
             </section>
