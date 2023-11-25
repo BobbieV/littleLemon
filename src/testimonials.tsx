73f0bae443e054f08e30ef307c16b0892c2747e1
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-11-24 16:49:25 -1000

    build out semantic html for testimonials.tsx

--- src/testimonials.tsx
+++ src/testimonials.tsx
@@ -7,15 +7,37 @@ function Testimonials() {
 
   return (
     <>
-      <div className="testimonialsWrapper">
-        <header>
-            <h1 className="title">Testimonials</h1>
-            <section></section>
-            <section></section>
-            <section></section>
-            <section></section>
+        <header id="testimonialsWrapper">
+            <h1 className="h1Testimonials">Testimonials</h1>
+            <section className="testimonialContainer testimonial1">
+              <img className="testimonialUserImage testimonialUserImage1"></img>
+              <hgroup className="h2TestimonialText">
+                  <h2 className="rating rating1"></h2>
+              </hgroup>
+              <p className="userFeedback userFeedback1"></p>
+             </section>
+            <section className="testimonialContainer testimonial2">
+            <img className="testimonialUserImage testimonialUserImage2"></img>
+              <hgroup className="h2TestimonialText">
+                <h2 className="rating rating2"></h2>
+              </hgroup>
+              <p className="userFeedback userFeedback2"></p>
+            </section>
+            <section className="testimonialContainer testimonial3">
+            <img className="testimonialUserImage testimonialUserImage3"></img>
+              <hgroup className="h2TestimonialText">
+                <h2 className="rating rating3"></h2>
+              </hgroup>
+              <p className="userFeedback userFeedback3"></p>
+            </section>
+            <section className="testimonialContainer testimonial4">
+            <img className="testimonialUserImage testimonialUserImage4"></img>
+              <hgroup className="h2TestimonialText">
+                <h2 className="rating rating4"></h2>
+              </hgroup>
+              <p className="userFeedback userFeedback4"></p>
+            </section>
             </header>
-        </div>
     </>
   )
 }
