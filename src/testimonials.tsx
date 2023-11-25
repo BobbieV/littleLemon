8741e11a3a4529675f1124cd6e067d9e3b61ab9f
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-11-24 16:52:42 -1000

    input rating numbers

--- src/testimonials.tsx
+++ src/testimonials.tsx
@@ -12,28 +12,28 @@ function Testimonials() {
             <section className="testimonialContainer testimonial1">
               <img className="testimonialUserImage testimonialUserImage1"></img>
               <hgroup className="h2TestimonialText">
-                  <h2 className="rating rating1"></h2>
+                  <h2 className="rating rating1">9/10</h2>
               </hgroup>
               <p className="userFeedback userFeedback1"></p>
              </section>
             <section className="testimonialContainer testimonial2">
             <img className="testimonialUserImage testimonialUserImage2"></img>
               <hgroup className="h2TestimonialText">
-                <h2 className="rating rating2"></h2>
+                <h2 className="rating rating2">10/10</h2>
               </hgroup>
               <p className="userFeedback userFeedback2"></p>
             </section>
             <section className="testimonialContainer testimonial3">
             <img className="testimonialUserImage testimonialUserImage3"></img>
               <hgroup className="h2TestimonialText">
-                <h2 className="rating rating3"></h2>
+                <h2 className="rating rating3">9/10</h2>
               </hgroup>
               <p className="userFeedback userFeedback3"></p>
             </section>
             <section className="testimonialContainer testimonial4">
             <img className="testimonialUserImage testimonialUserImage4"></img>
               <hgroup className="h2TestimonialText">
-                <h2 className="rating rating4"></h2>
+                <h2 className="rating rating4">8/10</h2>
               </hgroup>
               <p className="userFeedback userFeedback4"></p>
             </section>
