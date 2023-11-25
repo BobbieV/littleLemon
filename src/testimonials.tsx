382dd622376c1b246eb69cdcec2a048a3891d258
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-11-24 17:13:31 -1000

    input testimonial content

--- src/testimonials.tsx
+++ src/testimonials.tsx
@@ -15,7 +15,7 @@ function Testimonials() {
                 <h2 className="name name1">Joan</h2>
                 <h2 className="rating rating1">9/10</h2>
               </hgroup>
-              <p className="userFeedback userFeedback1"></p>
+              <p className="userFeedback userFeedback1">The food was outstanding and the atmosphere was lovely. The service was great too, but it was a little pricey. Overall, a fantastic experience.</p>
              </section>
             <section className="testimonialContainer testimonial2">
             <img className="testimonialUserImage testimonialUserImage2 testimonialUserImageDiane" src="https://github.com/BobbieV/littleLemon/blob/88e414dfd6b3636026a90dc465f15aaafc032289/src/assets/images/'Diane'%20Female%20Profile%20Pic%2075%20x%2075px.png?raw=true"></img>
@@ -23,7 +23,7 @@ function Testimonials() {
                 <h2 className="rating rating2">10/10</h2>
                 <h2 className="name name1">Diane</h2>
               </hgroup>
-              <p className="userFeedback userFeedback2"></p>
+              <p className="userFeedback userFeedback2">Everything was perfect! The food was absolutely delicious, the service was top-notch, and the atmosphere was wonderful. Highly recommend this restaurant.</p>
             </section>
             <section className="testimonialContainer testimonial3">
             <img className="testimonialUserImage testimonialUserImage3 testimonialUserImageDavid" src="https://github.com/BobbieV/littleLemon/blob/88e414dfd6b3636026a90dc465f15aaafc032289/src/assets/images/'David'%20Male%20Profile%20Pic%2075%20x%2075px.png?raw=true"></img>
@@ -31,7 +31,7 @@ function Testimonials() {
                 <h2 className="rating rating3">9/10</h2>
                 <h2 className="name name1">David</h2>
               </hgroup>
-              <p className="userFeedback userFeedback3"></p>
+              <p className="userFeedback userFeedback3">The food was great, but the service was a bit slow. The atmosphere was nice, but a little noisy. Still, a solid choice for a night out.</p>
             </section>
             <section className="testimonialContainer testimonial4">
             <img className="testimonialUserImage testimonialUserImage4 testimonialUserImageJenna" src="https://github.com/BobbieV/littleLemon/blob/88e414dfd6b3636026a90dc465f15aaafc032289/src/assets/images/'Jenna'%20Female%20Profile%20Pic%2075%20x%2075px.png?raw=true"></img>
@@ -39,7 +39,7 @@ function Testimonials() {
                 <h2 className="rating rating4">8/10</h2>
                 <h2 className="name name1">Jenna</h2>
               </hgroup>
-              <p className="userFeedback userFeedback4"></p>
+              <p className="userFeedback userFeedback4">The food was good, but nothing stood out as exceptional. The service was okay, but a little inattentive. The atmosphere was cozy, but a bit cramped.</p>
             </section>
             </header>
     </>
