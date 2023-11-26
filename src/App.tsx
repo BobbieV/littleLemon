4e47517b11a9986bb5c77d4e548449a273ee9484
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-11-26 08:05:28 -1000

    add missing about section 'about.tsx'

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@
 import  Header  from './Header.tsx'
 import  Navbar  from './navbar.tsx'
 import Specials from './specials.tsx'
+import About from './about.tsx'
 import Testimonials from './testimonials.tsx'
 import Footer from './footer.tsx'
+
 
 import './index.css'
 
@@ -17,6 +19,7 @@ function App() {
         <Header/>
         <Specials/>
         <Testimonials/>
+        <About/>
         <Footer/>
         </main>
     </>
---
+++ src/about.tsx
@@ -0,0 +1,22 @@
+
+
+
+function About() {
+
+
+  return (
+    <>
+        <header id="header">
+            <section className="textWrapper"><h1 className="title">Little Lemon</h1>
+            <h2 className="h2text text">Chicago</h2>
+            <p className="text ptext">Little Lemon Mediterranean is a family-owned restaurant in the heart of Chicago, serving delicious and authentic Mediterranean cuisine. Our menu features fresh ingredients, vibrant flavors, and traditional dishes that will transport your taste buds to the Mediterranean. Come visit us and taste the difference of homemade food made with love.</p>
+            </section>
+            <section className="heroImageWrapper">
+            <img src="https://github.com/BobbieV/littleLemon/blob/main/src/assets/images/Hero%20Image.png?raw=true" className="heroImage"></img>
+        </section>
+        </header>
+    </>
+  )
+}
+
+export default About;
