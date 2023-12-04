e89a98e068c6a583b2b21b1795998b667cf3d126
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-12-04 09:23:21 -1000

    hide breakword on order online

--- src/navbar.tsx
+++ src/navbar.tsx
@@ -14,7 +14,7 @@ function Navbar() {
             <li className="navbar navtextAbout" style={{/*width: 125, height: 100,*/ textAlign: 'center', color: '#495E57', fontSize: 18, fontFamily: 'Markazi Text', fontWeight: '400', wordWrap: 'break-word'}}>About</li>
             <li className="navbar navtextMenu" style={{/*width: 125, height: 100,*/ textAlign: 'center', color: '#495E57', fontSize: 18, fontFamily: 'Markazi Text', fontWeight: '500', wordWrap: 'break-word'}}>Menu</li>
             <li className="navbar navtextReservations" style={{/*width: 175, height: 100,*/ textAlign: 'center', color: '#495E57', fontSize: 18, fontFamily: 'Markazi Text', fontWeight: '500', wordWrap: 'break-word'}}>Reservations</li>
-            <li className="navbar navtextOrderonline" style={{/*width: 175, height: 100,*/ textAlign: 'center', color: '#495E57', fontSize: 18, fontFamily: 'Markazi Text', fontWeight: '500', wordWrap: 'break-word'}}>Order Online</li>
+            <li className="navbar navtextOrderonline" style={{/*width: 175, height: 100,*/ textAlign: 'center', color: '#495E57', fontSize: 18, fontFamily: 'Markazi Text', fontWeight: '500', /*wordWrap: 'break-word'*/}}>Order Online</li>
             <li className="navbar navtextLogin" style={{/*width: 125, height: 100,*/ textAlign: 'center', color: '#333333', fontSize: 18, fontFamily: 'Markazi Text', fontWeight: '500', wordWrap: 'break-word'}}>Login</li>
         </ul>
        </nav>
