15740b6f9d4f5d3071c1e6022b8b33063938ca31
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-12-04 09:34:47 -1000

    .title fontsize 6dvw

--- src/Header.tsx
+++ src/Header.tsx
@@ -7,7 +7,7 @@ function Header() {
   return (
     <>
       <header id="header" style={{/*width: 1440, height: 440,*/ background: '#495E57'}}>
-            <section className="textWrapper"><h1 className="title" style={{/*width: 341, height: 131,*/ textAlign: 'center', color: '#F4CE14', fontSize: 72, fontFamily: 'Markazi Text', fontWeight: '400', wordWrap: 'break-word'}}>Little Lemon</h1>
+            <section className="textWrapper"><h1 className="title" style={{/*width: 341, height: 131,*/ textAlign: 'center', color: '#F4CE14', /*fontSize: 72*/, fontFamily: 'Markazi Text', fontWeight: '400', wordWrap: 'break-word'}}>Little Lemon</h1>
             <h2 className="h2text text" style={{textAlign: 'center', color: '#EDEFEE', fontSize: 40, fontFamily: 'Karla', fontWeight: '400', wordWrap: 'break-word'}}>Chicago</h2>
             <p className="text ptext" style={{/*width: 486, height: 143,*/ color: '#EDEFEE', fontSize: 32, fontFamily: 'Karla', fontWeight: '400', wordWrap: 'break-word'}}>We are a family-owned Mediterranean restaurant, focused on traditional recipes, served with a modern twist.</p>
             <div className="reserveATableWrapper">
