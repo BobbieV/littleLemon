6ccfafc77e7bfaa3d7e2a2fe7c1d06383869e84d
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-12-07 09:04:43 -1000

    fix weird missing section tag

--- src/about.tsx
+++ src/about.tsx
@@ -12,7 +12,7 @@ function About() {
             <h2 className="h2text text" style={{/*width: '100%', height: '100%',*/ color: 'white', fontSize: 32, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Chicago</h2>
             <p className="text ptext" style={{/*width: '100%', height: '100%',*/ color: 'white', fontSize: 32, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Little Lemon Mediterranean is a family-owned restaurant in the heart of Chicago, serving delicious and authentic Mediterranean cuisine. Our menu features fresh ingredients, vibrant flavors, and traditional dishes that will transport your taste buds to the Mediterranean. Come visit us and taste the difference of homemade food made with love.</p>
             </section>
-            < className="heroImageWrapper">
+            <section className="heroImageWrapper">
             <img src="https://github.com/BobbieV/littleLemon/blob/main/src/assets/images/Hero%20Image.png?raw=true" className="heroImage" /*style={{width: '100%', height: '100%'}}*/></img>
         </section>
         </header>
