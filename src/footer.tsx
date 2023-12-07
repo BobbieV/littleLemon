6e89705f6ab3eb7bd70244a5781bce5291609575
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-12-07 08:43:05 -1000

    hide3 style tag AppLogo

--- src/footer.tsx
+++ src/footer.tsx
@@ -8,7 +8,7 @@ function Footer() {
   return (
     <>
         <footer id="#footer" style={{width: '100%', height: '100%', background: 'white'}}>
-          <img className="footerLogo" src="https://github.com/BobbieV/littleLemon/blob/83cc181e5814ff2107709d62e23e2ad9f0119ef0/src/assets/images/logo.png?raw=true" style={{width: '100%', height: '100%'}}></img>
+          <img className="footerLogo" src="https://github.com/BobbieV/littleLemon/blob/83cc181e5814ff2107709d62e23e2ad9f0119ef0/src/assets/images/logo.png?raw=true" /*style={{width: '100%', height: '100%'}}*/></img>
             <ul className="footer col1Footer" style={{/*width: '100%', height: '100%',*/ color: 'black', fontSize: 20, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>
                 <li>Doormat</li>
                 <li>Navigation</li>
