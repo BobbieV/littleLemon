4ce77273d8c7cefa7d84476d496887dfad75b66b
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-12-08 11:06:59 -1000

    change .textWrapper to .aboutTextWrapper for <About />

--- src/about.tsx
+++ src/about.tsx
@@ -7,7 +7,7 @@ function About() {
   return (
     <>
         <header id="about" /*style={{width: '100%', height: '100%', background: '#999999'}}*/>
-            <section className="textWrapper">
+            <section className="aboutTextWrapper">
             <h1 className="title" style={{/*width: '100%', height: '100%',*/ color: '#F4CE14', fontSize: 40, fontFamily: 'Markazi Text', fontWeight: '400', wordWrap: 'break-word'}}>Little Lemon</h1>
             <h2 className="h2text text" style={{/*width: '100%', height: '100%',*/ color: 'white', fontSize: 32, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Chicago</h2>
             <p className="text aboutPText" style={{/*width: '100%', height: '100%',*/ color: 'white', fontSize: 18, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Little Lemon Mediterranean is a family-owned restaurant in the heart of Chicago, serving delicious and authentic Mediterranean cuisine. Come visit us and taste the difference of homemade food made with love.</p>
