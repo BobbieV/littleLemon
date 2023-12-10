c0196e724949ef64c8bef0a9d4988c9d75055209
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-12-10 08:59:02 -1000

    put some alerts on coming soon features

--- src/footer.tsx
+++ src/footer.tsx
@@ -10,25 +10,25 @@ function Footer() {
         <footer id="#footer" /*style={{width: '100%', height: '100%', background: 'white'}}*/>
           <img className="footerLogo" src="https://github.com/BobbieV/littleLemon/blob/83cc181e5814ff2107709d62e23e2ad9f0119ef0/src/assets/images/logo.png?raw=true" /*style={{width: '100%', height: '100%'}}*/></img>
             <ul className="footer col1Footer" style={{/*width: '100%', height: '100%',*/ color: 'black', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>
-                <li>Doormat</li>
-                <li>Navigation</li>
-                <li>Home</li>
-                <li>About</li>
-                <li>Menu</li>
-                <li>Reservations</li>
-                <li>Order Online</li>
-                <li>Login</li>
+                <a>Doormat</a>
+                <a href="#nav">Navigation</a>
+                <a href="#nav">Home</a>
+                <a href="#nav">About</a>
+                <a href="#specials">Menu</a>
+                <a href="#header">Reservations</a>
+                <a onClick={() => alert('This feature is coming soon!')}>Order Online</a>
+                <a onClick={() => alert('This feature is coming soon!')}>Login</a>
             </ul>
             <ul className="footer col2Footer" style={{/*width: '100%', height: '100%',*/ color: 'black', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>
                 Contact
-                <li>Address</li>
-                <li>Phone Number</li>
-                <li>Email</li>
+                <a>Address</a>
+                <a>Phone Number</a>
+                <a>Email</a>
             </ul>
             <ul className="footer col3Footer" style={{/*width: '100%', height: '100%',*/ color: 'black', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Social Media
-                <li>Facebook</li>
-                <li>Instagram</li>
-                <li>TikTok</li>
+                <a href="www.facebook.com">Facebook</a>
+                <a href="www.instagram.com">Instagram</a>
+                <a href="www.tiktok.com">TikTok</a>
             </ul>
             </footer>
     </>
