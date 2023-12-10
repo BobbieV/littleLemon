64a7eae790600965493a537eca2522be3e2810fc
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-12-10 10:36:56 -1000

    still tinkering with <DatePicker />

--- src/datePicker.tsx
+++ src/datePicker.tsx
@@ -1,10 +1,14 @@
-import { useState } from 'react';
-import DatePicker from 'react-datepicker';
+import { useState } from "react";
+import DatePicker from "react-datepicker";
 
 () => {
     const [startDate, setStartDate] = useState(new Date());
     return (
-      <DatePicker selected={startDate} onChange={(date) => setStartDate(date)}>
+      <DatePicker 
+        selected={startDate} 
+        onChange={(date) => setStartDate(date)}
+        showTimeSelect
+        dateFormat="Pp">
         <div style={{ color: "red" }}>Don't forget to check the weather!</div>
       </DatePicker>
     );
