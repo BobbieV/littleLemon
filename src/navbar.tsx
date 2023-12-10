4901d123ea45fe33209f48baae9074a30d91317f
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-12-10 08:41:36 -1000

    test .navtexthome href="nav" about and specials

--- src/navbar.tsx
+++ src/navbar.tsx
@@ -10,12 +10,12 @@ function Navbar() {
         <nav id="nav">
           <img src="https://github.com/BobbieV/littleLemon/blob/main/src/assets/images/logo.png?raw=true" className="AppLogo navbar" /*style={{width: 294, height: 105, position: 'relative'}}*/></img>
           <ul className="navtext">
-            <li className="navbar navtextHome" style={{/*width: 125, height: 100,*/ textAlign: 'justify', color: '#495E57', fontSize: 12, fontFamily: 'Markazi Text', fontWeight: '500', wordWrap: 'break-word'}}>Home</li>
-            <li className="navbar navtextAbout" style={{/*width: 125, height: 100,*/ textAlign: 'justify', color: '#495E57', fontSize: 12, fontFamily: 'Markazi Text', fontWeight: '400', wordWrap: 'break-word'}}>About</li>
-            <li className="navbar navtextMenu" style={{/*width: 125, height: 100,*/ textAlign: 'justify', color: '#495E57', fontSize: 12, fontFamily: 'Markazi Text', fontWeight: '500', wordWrap: 'break-word'}}>Menu</li>
-            <li className="navbar navtextReservations" style={{/*width: 175, height: 100,*/ textAlign: 'justify', color: '#495E57', fontSize: 12, fontFamily: 'Markazi Text', fontWeight: '500', wordWrap: 'break-word'}}>Reservations</li>
-            <li className="navbar navtextOrderonline" style={{/*width: 175, height: 100,*/ textAlign: 'justify', color: '#495E57', fontSize: 12, fontFamily: 'Markazi Text', fontWeight: '500', /*wordWrap: 'break-word'*/}}>Order Online</li>
-            <li className="navbar navtextLogin" style={{/*width: 125, height: 100,*/ textAlign: 'justify', color: '#333333', fontSize: 12, fontFamily: 'Markazi Text', fontWeight: '500', wordWrap: 'break-word'}}>Login</li>
+            <a className="navbar navtextHome" href="#nav" style={{/*width: 125, height: 100,*/ textAlign: 'justify', color: '#495E57', fontSize: 12, fontFamily: 'Markazi Text', fontWeight: '500', wordWrap: 'break-word'}}>Home</a>
+            <a className="navbar navtextAbout" href="#about" style={{/*width: 125, height: 100,*/ textAlign: 'justify', color: '#495E57', fontSize: 12, fontFamily: 'Markazi Text', fontWeight: '400', wordWrap: 'break-word'}}>About</a>
+            <a className="navbar navtextMenu" href="#specials" style={{/*width: 125, height: 100,*/ textAlign: 'justify', color: '#495E57', fontSize: 12, fontFamily: 'Markazi Text', fontWeight: '500', wordWrap: 'break-word'}}>Menu</a>
+            <a className="navbar navtextReservations" style={{/*width: 175, height: 100,*/ textAlign: 'justify', color: '#495E57', fontSize: 12, fontFamily: 'Markazi Text', fontWeight: '500', wordWrap: 'break-word'}}>Reservations</a>
+            <a className="navbar navtextOrderonline" style={{/*width: 175, height: 100,*/ textAlign: 'justify', color: '#495E57', fontSize: 12, fontFamily: 'Markazi Text', fontWeight: '500', /*wordWrap: 'break-word'*/}}>Order Online</a>
+            <a className="navbar navtextLogin" style={{/*width: 125, height: 100,*/ textAlign: 'justify', color: '#333333', fontSize: 12, fontFamily: 'Markazi Text', fontWeight: '500', wordWrap: 'break-word'}}>Login</a>
         </ul>
        </nav>
     </>
