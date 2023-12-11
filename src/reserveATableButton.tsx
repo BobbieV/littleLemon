1666471a6c3901b168a5e4a5a4a99fc8dd56fd4b
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-12-11 11:41:20 -1000

    hide problematic onClick

--- src/reserveATableButton.tsx
+++ src/reserveATableButton.tsx
@@ -8,7 +8,7 @@ export default function reserveATableButton() {
     }*/
 
     return (
-      <button className="button reserveATableButton" onClick={alert('This feature is coming soon!')}>
+      <button className="button reserveATableButton" /*onClick={alert('This feature is coming soon!')}*/>
         Reserve a Table
       </button>
     );
