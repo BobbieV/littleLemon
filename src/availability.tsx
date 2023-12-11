3a771b55682e45449d160e8c11efcf9b94d79caf
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-12-11 09:56:30 -1000

    oops

--- src/availability.tsx
+++ src/availability.tsx
@@ -6,7 +6,7 @@ function Availability() {
 
     return (
     <>
-    <body id="availabilityBody"></body>
+    <body id="availabilityBody">
     <nav className="availabilityNav"><img src="https://github.com/BobbieV/littleLemon/blob/83cc181e5814ff2107709d62e23e2ad9f0119ef0/src/assets/images/logo.png?raw=true" className="availabilityLogo"></img></nav>
     <header className="availabilityHeader" style={{background: '#EDEFEE'}}>Check For Availability</header>
     <input type="date"></input>
