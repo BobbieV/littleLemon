5b3aacf0e1eeb1d7761f1dff542a7bf68015ebb2
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-12-10 17:37:30 -1000

    resolve errors

--- src/availability.tsx
+++ src/availability.tsx
@@ -1,12 +1,12 @@
 /*import DatePicker from react-DatePicker;*/
-import "index.css";
-import "logo.png";
+import './index.css';
+
 
 function Availability() {
 
     return (
     <>
-    <nav className="availabilityNav"><img src="logo.png" className="availabilityLogo"></img></nav>
+    <nav className="availabilityNav"><img src="https://github.com/BobbieV/littleLemon/blob/83cc181e5814ff2107709d62e23e2ad9f0119ef0/src/assets/images/logo.png?raw=true" className="availabilityLogo"></img></nav>
     <header className="availabilityHeader" style={{background: '#EDEFEE'}}>Check For Availability</header>
     <input type="date"></input>
     </>
