604908f59202216e0129c919049f00b4faa97e20
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-12-11 06:42:10 -1000

    place route in .reserveATable.tsx

--- src/reserveATableButton.tsx
+++ src/reserveATableButton.tsx
@@ -1,10 +1,25 @@
 import "./index.css";
 import  Availability from './availability.tsx';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+  } from "react-router-dom";
+  import Availability from './availability.tsx';
 
 export default function reserveATableButton() {
     function handleClick() {
         return (
-        <Availability/>)
+            <Router>
+                <Routes>
+                    <Route
+                        exact
+                        path="/"
+                        element={<Availability />} />
+                </Routes>
+            </Router>
+            )
     }
 
     return (
