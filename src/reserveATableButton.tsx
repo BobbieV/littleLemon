95ba287a243eef93080ccb6d843d3bd376aa826f
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-12-11 07:01:16 -1000

    remove all reactrouter

--- src/reserveATableButton.tsx
+++ src/reserveATableButton.tsx
@@ -1,23 +1,11 @@
 import "./index.css";
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-  } from "react-router-dom";
+
   import Availability from './availability.tsx';
 
 export default function reserveATableButton() {
     function handleClick() {
         return (
-            <Router>
-                <Routes>
-                    <Route
-                        exact
-                        path="/"
-                        element={<Availability />} />
-                </Routes>
-            </Router>
-            )
+            <Availability/>)
     }
 
     return (
