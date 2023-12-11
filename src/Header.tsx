7625aa589aef2d34ebf813e51487e59301555a2c
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-12-10 18:09:39 -1000

    remove erroneaous handleClick in Header

--- src/Header.tsx
+++ src/Header.tsx
@@ -13,9 +13,7 @@ function Header() {
               <p className="text ptext" style={{color: '#EDEFEE', fontFamily: 'Karla', fontWeight: '400', wordWrap: 'break-word'}}>We are a family-owned Mediterranean restaurant, focused on traditional recipes, served with a modern twist.</p>
               <div className="reserveATableWrapper">
                 <button className="reserveATable" style={{textAlign: 'center', color: '#333333', fontFamily: 'Markazi Text', fontWeight: '400', wordWrap: 'break-word'}}
-                  handleClick={() => {
-                    return <Availability/>
-}}>Reserve a Table</button>
+>Reserve a Table</button>
               </div>
             </section>
             <section className="heroImageWrapper">
