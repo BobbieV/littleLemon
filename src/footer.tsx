2d19cac397ea9fd58dd7eb0d180e6a0c070213dd
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-12-12 08:07:19 -1000

    clean up #footer

--- src/footer.tsx
+++ src/footer.tsx
@@ -9,7 +9,7 @@ function Footer() {
     <>
         <footer id="#footer">
           <img className="footerLogo" src="https://github.com/BobbieV/littleLemon/blob/83cc181e5814ff2107709d62e23e2ad9f0119ef0/src/assets/images/logo.png?raw=true" alt="A lemon icon next to the words 'Little Lemon'"></img>
-            <ul className="footer col1Footer" style={{color: 'black', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>
+            <ul className="footer">
                 <a>Doormat</a>
                 <a href="#nav">Navigation</a>
                 <a href="#nav">Home</a>
@@ -20,13 +20,13 @@ function Footer() {
                 <a >Login</a>
             </ul>
             <textarea className="footerTextGroup">
-              <ul className="footer col2Footer" style={{color: 'black', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>
+              <ul className="footer">
                 Contact
                 <a>Address</a>
                 <a>Phone Number</a>
                 <a>Email</a>
             </ul>
-            <ul className="footer col3Footer" style={{color: 'black', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Social Media
+            <ul className="footer">Social Media
                 <a href="https://www.facebook.com">Facebook</a>
                 <a href="https://www.instagram.com">Instagram</a>
                 <a href="https://www.tiktok.com/explore">TikTok</a>
