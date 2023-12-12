32f5e5021e7fe93a73598e783a2da9c672f27aa5
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-12-12 09:21:02 -1000

    try to find where it's broken

--- src/availability.tsx
+++ src/availability.tsx
@@ -1,4 +1,4 @@
-import './index.css';
+
 
 
 function Availability() {
@@ -6,9 +6,11 @@ function Availability() {
     return (
     <>
     <body id="availabilityBody">
-    <nav className="availabilityNav"><img src="https://github.com/BobbieV/littleLemon/blob/83cc181e5814ff2107709d62e23e2ad9f0119ef0/src/assets/images/logo.png?raw=true" className="availabilityLogo"></img></nav>
-    <header className="availabilityHeader" style={{background: '#EDEFEE'}}>Check For Availability</header>
-    <input type="date"></input>
+    <nav className="availabilityNav">
+        <img src="https://github.com/BobbieV/littleLemon/blob/83cc181e5814ff2107709d62e23e2ad9f0119ef0/src/assets/images/logo.png?raw=true" 
+        className="availabilityLogo"></img>
+        </nav>
+    <header className="availabilityHeader">Check For Availability</header>
     </body>
     </>
     )
