802c072ca58a3fc69702c3755e7f774d1f88333a
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-12-15 08:59:37 -1000

    test react-router install

--- src/availability.tsx
+++ src/availability.tsx
@@ -7,7 +7,7 @@ function Availability() {
     <>
     <body id="availabilityBody">
     <nav className="availabilityNav">
-        <img src="https://github.com/BobbieV/littleLemon/blob/83cc181e5814ff2107709d62e23e2ad9f0119ef0/src/assets/images/logo.png?raw=true" 
+        <img src="https://github.com/BobbieV/littleLemon/blob/83cc181e5814ff2107709d62e23e2ad9f0119ef0/src/assets/images/logo.png?raw=true"
         className="availabilityLogo"></img>
         </nav>
     <header className="availabilityHeader">Check For Availability</header>
