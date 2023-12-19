0712d833d375594d3d49240296e774138b27f660
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-12-19 07:57:05 -1000

    wrap all<Navbar /> <a> in <li> per chatGPT

--- src/navbar.tsx
+++ src/navbar.tsx
@@ -10,12 +10,12 @@ function Navbar() {
         <nav id="nav">
           <img src="https://github.com/BobbieV/littleLemon/blob/main/src/assets/images/logo.png?raw=true" className="AppLogo" alt="A lemon icon followed by the words 'Little Lemon'"></img>
           <ul className="navtext">
-            <a className="navbar navtextHome" href="#nav">Home</a>
-            <a className="navbar navtextAbout" href="#about">About</a>
-            <a className="navbar navtextMenu" href="#specials">Menu</a>
-            <a className="navbar navtextReservations">Reservations</a>
-            <a className="navbar navtextOrderonline">Order Online</a>
-            <a className="navbar navtextLogin">Login</a>
+            <li><a className="navbar navtextHome" href="#nav">Home</a></li>
+            <li><a className="navbar navtextAbout" href="#about">About</a></li>
+            <li><a className="navbar navtextMenu" href="#specials">Menu</a></li>
+            <li><a className="navbar navtextReservations">Reservations</a></li>
+            <li><a className="navbar navtextOrderonline">Order Online</a></li>
+            <li><a className="navbar navtextLogin">Login</a></li>
         </ul>
        </nav>
     </>
