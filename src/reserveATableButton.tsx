9fc3041d78dca1982ef4c3d8b68c6566f15e8944
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-12-19 08:42:46 -1000

    unhide onClick in <Specials /> and duplicate it into <ReserveATable />

--- src/reserveATableButton.tsx
+++ src/reserveATableButton.tsx
@@ -2,7 +2,7 @@
 
 export default function ReserveATableButton() {
     return (
-      <button className="button reserveATableButton">
+      <button className="button reserveATableButton" onClick={() => alert('This feature is coming soon!')}>
         Reserve a Table
       </button>
     );
