d5ad101e2748643f3dac0e677e2c908963ccae3d
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-12-19 08:27:52 -1000

    add <lu> around all <a> in <Footer />

--- src/footer.tsx
+++ src/footer.tsx
@@ -8,28 +8,60 @@ function Footer() {
   return (
     <>
         <footer id="#footer">
-          <img className="footerLogo" src="https://github.com/BobbieV/littleLemon/blob/83cc181e5814ff2107709d62e23e2ad9f0119ef0/src/assets/images/logo.png?raw=true" alt="A lemon icon next to the words 'Little Lemon'"></img>
+          <img
+            className="footerLogo"
+            src="https://github.com/BobbieV/littleLemon/blob/83cc181e5814ff2107709d62e23e2ad9f0119ef0/src/assets/images/logo.png?raw=true"
+            alt="A lemon icon next to the words 'Little Lemon'">
+            </img>
             <ul className="footer">
+              <li>
                 <a>Doormat</a>
+              </li>
+              <li>
                 <a href="#nav">Navigation</a>
+              </li>
+              <li>
                 <a href="#nav">Home</a>
+              </li>
+              <li>
                 <a href="#nav">About</a>
+              </li>
+              <li>
                 <a href="#specials">Menu</a>
+              </li>
+              <li>
                 <a href="#header">Reservations</a>
+              </li>
+              <li>
                 <a >Order Online</a>
+              </li>
+              <li>
                 <a >Login</a>
+            </li>
             </ul>
             <textarea className="footerTextGroup">
               <ul className="footer">
                 Contact
-                <a>Address</a>
-                <a>Phone Number</a>
-                <a>Email</a>
+                <li>
+                  <a>Address</a>
+                </li>
+                <li>
+                  <a>Phone Number</a>
+                </li>
+                <li>
+                  <a>Email</a>
+                </li>
             </ul>
             <ul className="footer">Social Media
+              <li>
                 <a href="https://www.facebook.com">Facebook</a>
+              </li>
+              <li>
                 <a href="https://www.instagram.com">Instagram</a>
+              </li>
+              <li>
                 <a href="https://www.tiktok.com/explore">TikTok</a>
+              </li>
             </ul>
             </textarea>
             </footer>
