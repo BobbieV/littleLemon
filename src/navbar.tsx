32f78c69aa4a23d3bdbd649b17b85d73c2fc51aa
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2023-12-19 07:58:42 -1000

    clean up

--- src/navbar.tsx
+++ src/navbar.tsx
@@ -10,12 +10,24 @@ function Navbar() {
         <nav id="nav">
           <img src="https://github.com/BobbieV/littleLemon/blob/main/src/assets/images/logo.png?raw=true" className="AppLogo" alt="A lemon icon followed by the words 'Little Lemon'"></img>
           <ul className="navtext">
-            <li><a className="navbar navtextHome" href="#nav">Home</a></li>
-            <li><a className="navbar navtextAbout" href="#about">About</a></li>
-            <li><a className="navbar navtextMenu" href="#specials">Menu</a></li>
-            <li><a className="navbar navtextReservations">Reservations</a></li>
-            <li><a className="navbar navtextOrderonline">Order Online</a></li>
-            <li><a className="navbar navtextLogin">Login</a></li>
+            <li>
+              <a className="navbar navtextHome" href="#nav">Home</a>
+            </li>
+            <li>
+              <a className="navbar navtextAbout" href="#about">About</a>
+            </li>
+            <li>
+              <a className="navbar navtextMenu" href="#specials">Menu</a>
+            </li>
+            <li>
+              <a className="navbar navtextReservations">Reservations</a>
+            </li>
+            <li>
+              <a className="navbar navtextOrderonline">Order Online</a>
+            </li>
+            <li>
+              <a className="navbar navtextLogin">Login</a>
+            </li>
         </ul>
        </nav>
     </>
