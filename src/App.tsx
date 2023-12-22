b892bdeb469485067fbe041da6dd87ddbb163c1e
BobbieV <bobbiev@hawaii.edu>
2023-12-21 18:15:18 -1000

    remove creatBrowRou + createRouFromEle per ChatGPT

--- src/App.tsx
+++ src/App.tsx
@@ -9,24 +9,15 @@ import {
 } from 'react-router-dom';
 import Availability from './routes/availability.tsx';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Routes>
-            <Route path="/" element={ <Home />}/>
-            <Route path="/availability" element={ <Availability /> }/>
-          </Routes>
-  )
-)
 
 function App() {
 
   return (
     <>
-      <RouterProvider router={router}/>
-          <Routes>
-            <Route path="/" element={ <Home />}/>
-            <Route path="/availability" element={ <Availability /> }/>
-          </Routes>
+       <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/availability" element={<Availability /> }/>
+        </Routes>
     </>
   )
 }
