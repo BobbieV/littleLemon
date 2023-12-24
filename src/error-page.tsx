6996c18be5b32fc2e1b4376520507124e1c01fb1
BobbieV <bobbiev@hawaii.edu>
2023-12-24 13:31:29 -1000

    increase padding #availability className not classID .oops

--- src/error-page.tsx
+++ src/error-page.tsx
@@ -4,7 +4,7 @@ export default function ErrorPage() {
 
     return (
         <div id="error-page">
-            <h1 classId="oops">Oops!</h1>
+            <h1 className="oops">Oops!</h1>
             <p className="sorryText">Sorry, an unexpected error has occurred.</p>
         </div>
     )
