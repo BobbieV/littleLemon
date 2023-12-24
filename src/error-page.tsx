d27f93ca19801a27e27ba205bd7b2da120b028b4
BobbieV <bobbiev@hawaii.edu>
2023-12-24 13:29:44 -1000

    .oops .sorryText alignment

--- src/error-page.tsx
+++ src/error-page.tsx
@@ -4,8 +4,8 @@ export default function ErrorPage() {
 
     return (
         <div id="error-page">
-            <h1>Oops!</h1>
-            <p>Sorry, an unexpected error has occurred.</p>
+            <h1 classId="oops">Oops!</h1>
+            <p className="sorryText">Sorry, an unexpected error has occurred.</p>
         </div>
     )
 }
