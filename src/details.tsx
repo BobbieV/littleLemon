dff5c2e5a533499d4f585fb874350575f61d7b15
BobbieV <bobbiev@hawaii.edu>
2023-12-26 13:21:18 -1000

    remove errant text from input

--- src/details.tsx
+++ src/details.tsx
@@ -10,23 +10,23 @@ function Details() {
             <header className="detailsHeader">
                 <h2 className="detailsH2">Enter Your Details</h2>
             </header>
-            <form id="detailsForm1">
-                <label className="nameLabel">Name:
-                </label>
-                <input type="text">Name:</input>
-                <label className="phoneLabel">Phone Number:
-                </label>
-                <input type="tel"></input>
-                <label className="addtlInfo">Anything you would like us to know? e.g. food allergies, special events?
-                </label>
-                <input type="text"></input>
-                <label className="referral">How did you hear about us?</label>
-                <input type="text"></input>
-                <div className="detailsSubmit">
-                    <Link to="/review" id="detailsNextPage" className="button">Next Page</Link>
-                </div>
+                <form id="detailsForm1">
+                    <label className="nameLabel">Name:
+                    </label>
+                    <input type="text"></input>
+                    <label className="phoneLabel">Phone Number:
+                    </label>
+                    <input type="tel"></input>
+                    <label className="addtlInfo">Anything you would like us to know? e.g. food allergies, special events?
+                    </label>
+                    <input type="text"></input>
+                    <label className="referral">How did you hear about us?</label>
+                    <input type="text"></input>
+                    <div className="detailsSubmit">
+                        <Link to="/review" id="detailsNextPage" className="button">Next Page</Link>
+                    </div>
 
-            </form>
+                </form>
             </main>
         </>
     )
