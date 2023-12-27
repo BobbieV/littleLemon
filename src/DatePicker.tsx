b32dc1a0e03c923fe4f1201bcf511c0c09bbbbf3
BobbieV <bobbiev@hawaii.edu>
2023-12-26 14:27:44 -1000

    hide showTimeSelect datePicker.tsx remove time

--- src/DatePicker.tsx
+++ src/DatePicker.tsx
@@ -27,9 +27,9 @@ const DatePickerComponent: React.FC<DatePickerComponentProps> = ({
 return (
     <div className="availInput inputDateWrapper ">
 
-        <label>Date & Time of Reservation:</label>
+        <label>Date of Reservation:</label>
         {props.selected !== null ? (
-            <p className="selectedDate">Selected Date/Time: {props.selected?.toLocaleString()}</p>
+            <p className="selectedDate">Selected Date: {props.selected?.toLocaleString()}</p>
              ) : null}
         <FontAwesomeIcon
                 icon={faCalendar}
@@ -38,7 +38,7 @@ return (
         />
         {showDatePicker && (
             <ReactDatePicker
-                showTimeSelect
+                //showTimeSelect
                 dateFormat="Pp"
                 selected={props.selected}
                 onChange={(date: Date | null) =>{
