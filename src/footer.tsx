95320682da331432b83e30616e05bd755b494df2
BobbieV <bobbiev@hawaii.edu>
2023-12-30 18:06:15 -1000

    hide yesterday footer Link mods

--- src/footer.tsx
+++ src/footer.tsx
@@ -14,7 +14,7 @@ function Footer() {
             alt="A lemon icon next to the words 'Little Lemon'">
             </img>
           <div className="footerTextGroup">
-            <ul className="footer">
+{/*}            <ul className="footer">
               <li>Doormat</li>
               <li>Navigation</li>
               <li>
@@ -33,7 +33,7 @@ function Footer() {
                 <Link to="/orderOnline">Order Online</Link></li>
               <li>
                 <Link to="/login">Login</Link></li>
-            </ul>
+  </ul>*/}
               <ul className="footer">
                 Contact
                 <li>Address</li>
