d11a72cf9defee56aff4e0c7d2bf17dd500b3d8c
BobbieV <bobbiev@hawaii.edu>
2023-12-30 14:18:46 -1000

    Phase 1 Formik implementation

--- src/availability.tsx
+++ src/availability.tsx
@@ -1,16 +1,11 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faQuestion } from '@fortawesome/free-solid-svg-icons';
-import  DatePickerComponent from './DatePicker.tsx';
-import { useState } from 'react';
+import { faCalendar, faQuestion } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
+import { Formik, Form, Field} from 'formik'
 
 
+const Availability = () => {
 
-function Availability() {
-    const [selectedDate, setSelectedDate] = useState<Date | null>(null);
-    const handleDateSelect = (date: Date | null) => {
-        setSelectedDate(date);
-    };
 
     return(
     <>
@@ -21,38 +16,61 @@ function Availability() {
                 <h2 className="availH2">For</h2>
                 <h2 className="availH2">Availability</h2>
             </header>
-            <form id="availForm1">
-                    <DatePickerComponent
-                        onChange ={(date: Date | null) =>{
-                            handleDateSelect(date);
-                        }}
-                        selected={selectedDate}
-                        handleDateSelect={handleDateSelect}
-                        required
-                        />
-
+            <Formik
+                initialValues={{
+                    numGuests: "",
+                    resDateTime: ""
+                }}
+                onSubmit ={(values) => {
+            alert(JSON.stringify(values))
+                }}
+            >
+                { ({handleSubmit, values, handleChange}) => (
+                    <Form id="availForm1" onSubmit={handleSubmit}>
+<div className ="availInput inputNumGuestsWrapper">
+                <label
+                    htmlFor="resDateTime"
+                    >Reservation Date & Time:
+                    </label>
+                <Field id="resDateTime" name="resDateTime">
+                <input
+                    value={values.resDateTime}
+                    name="inputresDateTime"
+                    required
+                    onChange={handleChange}
+                />
+                <FontAwesomeIcon
+                    icon={ faCalendar }
+                    className="fa icon"
+                    aria-hidden="true"
+                    onClick={() => alert('For Parties of more than 8 people, please call the restaurant directly to make reservations.')}></FontAwesomeIcon></Field>
+            </div>
             <div className ="availInput inputNumGuestsWrapper">
                 <label
-                    className="guestsLabel"
+                    htmlFor="numGuests"
                     >Number of Guests:
                     </label>
+                <Field id="numGuests" name="numGuests">
                 <input
                     type="number"
                     className="inputNumGuests"
                     required
+                    value={values.numGuests}
+                    onChange={handleChange}
                     min="1"
-                    max="8">
-                </input>
+                    max="8" />
                 <FontAwesomeIcon
                     icon={ faQuestion }
                     className="fa icon"
                     aria-hidden="true"
-                    onClick={() => alert('For Parties of more than 8 people, please call the restaurant directly to make reservations.')}></FontAwesomeIcon>
+                    onClick={() => alert('For Parties of more than 8 people, please call the restaurant directly to make reservations.')}></FontAwesomeIcon></Field>
             </div>
             <div className="availSubmit">
                 <Link to="/details" id="availNextPage" className="button">Next Page</Link>
             </div>
-        </form>
+        </Form>
+    )}
+        </Formik>
     </main>
     </>
    ) }
